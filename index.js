676739e17e3cf3c6dd70dc148f193678d212d7e0
Vasil Gudzhev <vgudzhev@gmail.com>
2017-10-08 15:33:22 +0300

    Update Readme

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var validator = require('./scripts/validator.js')();
 var generator = require('./scripts/generator.js')();
 
-module.exports = function() {
-    this.validate = require('./scripts/validator.js')(egn);
-}
+
