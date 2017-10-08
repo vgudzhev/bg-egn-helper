a46927f405d9c5e5291f8f1fabbd4520e1a53756
Vasil Gudzhev <vgudzhev@gmail.com>
2017-10-09 00:27:50 +0300

    Fix tests in index.js

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ const parser = require('./scripts/parser.js')();
 module.exports = validator;
 module.exports = generator;
 module.exports = parser;
-
-var a = generate();
-console.log(a);
-console.log(parse(a));
